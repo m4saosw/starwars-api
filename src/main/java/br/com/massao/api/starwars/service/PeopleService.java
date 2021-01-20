c6d6bf5df6b2f68898f2bf71d133677311f97bbf
Massao <lmassao@yahoo.com.br>
2021-01-20 03:21:59 -0300

    Adds service layer of people

---
+++ src/main/java/br/com/massao/api/starwars/service/PeopleService.java
@@ -0,0 +1,29 @@
+package br.com.massao.api.starwars.service;
+
+import br.com.massao.api.starwars.model.PersonModel;
+import br.com.massao.api.starwars.repository.PeopleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class PeopleService {
+
+    @Autowired
+    private PeopleRepository repository;
+
+    /**
+     * @return
+     */
+    public List<PersonModel> list() {
+        log.debug("list");
+
+        return repository.findAll();
+//        List<PersonModel> list = new ArrayList<>();
+//        list.add(PersonModel.builder().id(1L).birth_year("XFDFD").gender("man").height(123).homeworld("terra").mass(50).name("person").build());
+//        return list;
+    }
+}
