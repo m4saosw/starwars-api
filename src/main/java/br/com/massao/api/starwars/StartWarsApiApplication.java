94bc08d2f67ca71b0ab0511d56ba85a59d95836e
Massao <lmassao@yahoo.com.br>
2021-01-20 03:30:28 -0300

    Defines a new record in the database on startup (only during development)

--- src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
+++ src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
@@ -1,13 +1,26 @@
 package br.com.massao.api.starwars;
 
+import br.com.massao.api.starwars.model.PersonModel;
+import br.com.massao.api.starwars.repository.PeopleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StartWarsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StartWarsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StartWarsApiApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner init(PeopleRepository repository) {
+        return (args) -> {
+            PersonModel person = PersonModel.builder().birth_year("XFDSR").gender("man").height(123).homeworld("terra").mass(50).name("person1").build();
+            repository.save(person);
+        };
+    }
 
 }
