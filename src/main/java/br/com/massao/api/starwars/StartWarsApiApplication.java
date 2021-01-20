9f85c9b2444caf2fddd0cda15636ed54337494f9
Massao <lmassao@yahoo.com.br>
2021-01-20 01:12:41 -0300

    Creates initial structure

---
+++ src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.massao.api.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartWarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartWarsApiApplication.class, args);
+	}
+
+}
