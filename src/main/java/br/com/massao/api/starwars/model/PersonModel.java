e505a39e4006572262e738cfdf95f5296d492d46
Massao <lmassao@yahoo.com.br>
2021-01-20 03:20:45 -0300

    Adds entity of people

---
+++ src/main/java/br/com/massao/api/starwars/model/PersonModel.java
@@ -0,0 +1,49 @@
+package br.com.massao.api.starwars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor // used by JPA
+@AllArgsConstructor
+@Builder
+@Entity(name = "person")
+public class PersonModel {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name is empty")
+    private String name;
+
+    @Column(name = "height")
+    @Min(0)
+    private int height;
+
+    @Column(name = "mass")
+    @Min(0)
+    private int mass;
+
+    @Column(name = "birth_year")
+    @NotEmpty(message = "Birth_year is empty")
+    private String birth_year;
+
+    @Column(name = "gender")
+    @NotEmpty(message = "Gender is empty")
+    private String gender;
+
+    @Column(name = "homeworld")
+    @NotEmpty(message = "Homeworld is empty")
+    private String homeworld;
+
+}
