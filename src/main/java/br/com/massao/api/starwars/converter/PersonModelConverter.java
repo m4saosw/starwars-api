5cc6a6a076aa0c736c451c5f499470df789f1bcf
Massao <lmassao@yahoo.com.br>
2021-01-22 00:32:10 -0300

    Adds conversor dto to model

---
+++ src/main/java/br/com/massao/api/starwars/converter/PersonModelConverter.java
@@ -0,0 +1,22 @@
+package br.com.massao.api.starwars.converter;
+
+
+import br.com.massao.api.starwars.dto.PersonDto;
+import br.com.massao.api.starwars.model.PersonModel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class PersonModelConverter {
+
+    public PersonModel modelFrom(PersonDto person) {
+        PersonModel model = PersonModel.builder()
+                .name(person.getName())
+                .height(person.getHeight())
+                .mass(person.getMass())
+                .birth_year(person.getBirth_year())
+                .gender(person.getGender())
+                .homeworld(Integer.toString(person.getHomeworld()))
+                .build();
+        return model;
+    }
+}
