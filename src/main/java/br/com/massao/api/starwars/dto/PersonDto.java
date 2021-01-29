fbbfdcb02be3f6c775eca5d72694bd5eb4279508
Massao <lmassao@yahoo.com.br>
2021-01-28 21:01:58 -0300

    Adds resource level validation

--- src/main/java/br/com/massao/api/starwars/dto/PersonDto.java
+++ src/main/java/br/com/massao/api/starwars/dto/PersonDto.java
@@ -2,7 +2,11 @@ package br.com.massao.api.starwars.dto;
 
 import br.com.massao.api.starwars.model.PersonModel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @NoArgsConstructor // Used by Jackson
 @AllArgsConstructor
@@ -11,20 +15,34 @@ import lombok.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public class PersonDto {
+    @JsonProperty("id")
     private Long id;
 
+    @JsonProperty("name")
+    @NotEmpty(message = "Name is empty")
     private String name;
 
+    @JsonProperty("height")
+    @Min(0)
     private int height;
 
+    @JsonProperty("mass")
+    @Min(0)
     private int mass;
 
+    @JsonProperty("birth_year")
+    @NotEmpty(message = "birth_year is empty")
     private String birth_year;
 
+    @JsonProperty("gender")
+    @NotEmpty(message = "gender is empty")
     private String gender;
 
+    @JsonProperty("homeworld")
+    @Min(0)
     private int homeworld;
 
+    @JsonProperty("filmsExhibitions")
     private int filmsExhibitions; // TODO - query
 
     public PersonDto(PersonModel model) {
