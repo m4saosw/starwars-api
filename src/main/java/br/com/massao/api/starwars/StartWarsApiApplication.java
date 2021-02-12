d29f215d0a7437b7b543143389b9d8e10da7a7fd
Massao <lmassao@yahoo.com.br>
2021-02-12 00:38:10 -0300

    Improves performance by caching http request to Swapi API

--- src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
+++ src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
@@ -5,8 +5,10 @@ import br.com.massao.api.starwars.v1.repository.PeopleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+@EnableCaching
 @SpringBootApplication
 public class StartWarsApiApplication {
 
--- src/main/java/br/com/massao/api/starwars/v1/service/SwapiPlanetsService.java
+++ src/main/java/br/com/massao/api/starwars/v1/service/SwapiPlanetsService.java
@@ -5,13 +5,16 @@ import br.com.massao.api.starwars.v1.swapi.PlanetSwapiResults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -25,24 +28,45 @@ public class SwapiPlanetsService {
 
 
     public boolean existsPlanetByName(String name) {
-        final String url = endpoint.concat("/planets/?search=").concat(name);
+        log.debug("existsPlanetByName name={}", name);
+        return findPlanet(name) != null;
+    }
 
-        log.debug("findByName name={} url={}", name, url);
+    @Cacheable(value = "planetCache", unless = "#result != null")
+    private String findPlanetOld(String name) {
+        Instant now = Instant.now();
+        final String url = endpoint.concat("/planets/?search=").concat(name);
+        log.debug("findPlanet name={} url={}", name, url);
 
         ResponseEntity<PlanetSwapi> responses = restTemplate.getForEntity(url, PlanetSwapi.class);
+        log.debug("findPlanet name={} url={} results={} time={} ns", name, url, responses.getBody(), Instant.from(now).getNano());
+        return responses.hasBody() ? responses.getBody().getResults().get(0).getName() : null;
+    }
 
-        // Usando o metodo exchange
-        // Obtem apenas o primeiro encontrado
-        return responses.hasBody() ? responses.getBody().getCount() > 0 : false;
+    @Cacheable(value = "planetCache", unless = "#result != null")
+    private String findPlanet(String name) {
+        Instant now = Instant.now();
+        log.debug("findPlanet name={}", name);
+
+        List<PlanetSwapiResults> results = listAllPlanets();
+
+
+        //log.debug("findPlanet name={} results={} time={} ns", name, responses.getBody(), Instant.from(now).getNano());
+        log.debug("findPlanet name={} time={} ns", name, Instant.from(now).getNano());
+        //return responses.hasBody() ? responses.getBody().getResults().get(0).getName() : null;
+        List<PlanetSwapiResults> list = results.stream().filter(planet -> planet.getName().equals(name)).collect(Collectors.toList());
+        return list.isEmpty() ? null : list.get(0).getName();
     }
 
 
     // TODO - adicionar busca resiliente, permitindo retorno parcial de sucesso
     // TODO - adicionar informacoes no log em caso de retorno parcial
     // TODO - refatorar
+    @Cacheable(value = "planetsCache")
     public List<PlanetSwapiResults> listAllPlanets() {
-        String url = endpoint.concat("/planets/");
+        Instant now = Instant.now();
 
+        String url = endpoint.concat("/planets/");
         log.debug("listPlanets url={}", url);
 
         HttpHeaders headers = new HttpHeaders();
@@ -75,7 +99,8 @@ public class SwapiPlanetsService {
 
         } while (hasMorePages);
 
-        log.debug("listAllPlanets results={} resultsSize={}", results, results.size());
+
+        log.debug("listAllPlanets results={} resultsSize={} time={} ns", results, results.size(), Instant.from(now).getNano());
         return results;
     }
 
