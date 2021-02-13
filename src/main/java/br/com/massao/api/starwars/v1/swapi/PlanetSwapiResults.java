a385ba3d20955bcc1183045c515296a6dce3d56c
Massao <lmassao@yahoo.com.br>
2021-02-13 01:32:02 -0300

    Adds equals method

--- src/main/java/br/com/massao/api/starwars/v1/swapi/PlanetSwapiResults.java
+++ src/main/java/br/com/massao/api/starwars/v1/swapi/PlanetSwapiResults.java
@@ -3,6 +3,7 @@ package br.com.massao.api.starwars.v1.swapi;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @ToString
@@ -21,5 +22,20 @@ public class PlanetSwapiResults {
     @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlanetSwapiResults that = (PlanetSwapiResults) o;
+
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
