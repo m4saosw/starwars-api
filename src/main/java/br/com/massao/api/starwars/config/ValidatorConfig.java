9316a68e06a869ed36b5b62a3a1daa68113fa40e
Massao <lmassao@yahoo.com.br>
2021-02-17 20:01:30 -0300

    Removes unused imports

--- src/main/java/br/com/massao/api/starwars/config/ValidatorConfig.java
+++ src/main/java/br/com/massao/api/starwars/config/ValidatorConfig.java
@@ -1,15 +1,11 @@
 package br.com.massao.api.starwars.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.validation.Validator;
-import java.time.Duration;
 
 @EnableWebMvc
 @Configuration
