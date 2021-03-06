fec4971d7c58f9eee8241634e8cfcc4114fc8208
Massao <lmassao@yahoo.com.br>
2021-03-05 21:39:37 -0300

    Adds support to documentation with swagger

--- src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
+++ src/main/java/br/com/massao/api/starwars/StartWarsApiApplication.java
@@ -3,11 +3,12 @@ package br.com.massao.api.starwars;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCaching
 @SpringBootApplication
 //@EnableSpringDataWebSupport // receber os parâmetros de ordenação e paginação diretamente nos métodos do controller
+@EnableSwagger2
 public class StartWarsApiApplication {
 
     public static void main(String[] args) {
