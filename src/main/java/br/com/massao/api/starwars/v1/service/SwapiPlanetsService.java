4da23cda1e13b1d6eb95a5b58653c73a36ddbfe2
Massao <lmassao@yahoo.com.br>
2021-03-06 21:56:47 -0300

    Transforms implementation of swapi planets service into an interface to improve code decoupling

--- src/main/java/br/com/massao/api/starwars/v1/service/SwapiPlanetsService.java
+++ src/main/java/br/com/massao/api/starwars/v1/service/SwapiPlanetsService.java
@@ -1,90 +1,13 @@
 package br.com.massao.api.starwars.v1.service;
 
-import br.com.massao.api.starwars.v1.swapi.PlanetSwapi;
 import br.com.massao.api.starwars.v1.swapi.PlanetSwapiResults;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-@Slf4j
 @Service
-// TODO - cache da lista de planetas
-public class SwapiPlanetsService {
-    @Value("${swapi.url}")
-    private String endpoint;
+public interface SwapiPlanetsService {
+    boolean existsPlanetByName(String name);
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    // Auto referencia para funcionamento correto do proxy do cache, quando acionado por um metodo interno
-    @Resource
-    private SwapiPlanetsService self;
-
-
-    @Cacheable(value = "planet")
-    public boolean existsPlanetByName(String name) {
-        log.debug("existsPlanetByName name={}", name);
-
-        // TODO - conversao em set pode ser colocada em outro cache
-        Set<PlanetSwapiResults> setPlanets = self.listAllPlanets().stream().filter(planet -> planet.getName().equals(name)).collect(Collectors.toSet());
-
-        log.debug("existsPlanetByName name={} found={} elapsedTime={} ms", name, setPlanets.size());
-        return ! setPlanets.isEmpty();
-    }
-
-
-    // TODO - adicionar busca resiliente, permitindo retorno parcial de sucesso
-    // TODO - adicionar informacoes no log em caso de retorno parcial
-    // TODO - refatorar
-    @Cacheable(value = "planets")
-    public List<PlanetSwapiResults> listAllPlanets() {
-        Instant instant = Instant.now();
-
-        String url = endpoint.concat("/planets/");
-        log.debug("listPlanets url={}", url);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-
-
-        List<PlanetSwapiResults> results = new ArrayList<>();
-        boolean hasMorePages = true;
-
-        // leitura em blocos por pagina
-        do {
-            log.debug("listAllPlanets lendo url={}", url);
-
-            final ResponseEntity<PlanetSwapi> responses = restTemplate.exchange(url, HttpMethod.GET, entity,
-                    new ParameterizedTypeReference<PlanetSwapi>() {
-                    });
-
-            final PlanetSwapi body = responses.getBody();
-            if (body == null || body.getResults().isEmpty())
-                break;
-
-            results.addAll(body.getResults());
-
-            // Prepara a leitura da proxima pagina
-            if (body.getNext() != null)
-                url = body.getNext().replace("http", "https");
-            else
-                hasMorePages = false;
-
-        } while (hasMorePages);
-
-        return results;
-    }
+    List<PlanetSwapiResults> listAllPlanets();
 }
